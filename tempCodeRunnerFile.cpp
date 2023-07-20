f7aa2163dddec78d2620dd207f4c138b293dd41c
iamaryankr <aryankumar12003@gmail.com>
2023-07-20 13:33:12 +0530

    dsa

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,14 +1,144 @@
 #include<iostream>
 using namespace std;
-int main(){
 
-    int n=10;
-    int A[n]={1,2,4,5,6,7,-1,9,3,11};
-    int min=A[0]; int max=A[0];
+//Diagonal matrix
+class Diagonal{
+    private:
+         int n;int *A;
+    public:
+         Diagonal(int n){
+            this->n=n;
+            A=new int [n];
+         }
+         void Set(int i,int j,int x);
+         int Get(int i,int j);
+         void Display();
+         ~Diagonal(){
+            delete []A;
+         }
+};
+void Diagonal::Set(int i,int j,int x){
+    if(i==j) A[i-1]=x;
+}
+int Diagonal::Get(int i ,int j){
+    if(i==j) return A[i-1];
+    else return 0;
+}
+void Diagonal::Display(){
     for(int i=0;i<n;i++){
-        if(A[i]>max) max=A[i];
-        if(A[i]<min) min=A[i];
+        for(int j=0;j<n;j++){
+            if(i==j) cout << A[i-1];
+            else cout<<"0";
+        }
+        cout<<endl;
     }
-    cout<<max<<" "<<min;
+}
+
+class LTMatrix{
+    private:
+        int n;
+        int *A;
+    public:
+        LTMatrix(int n){
+            this->n=n;
+            A= new int [n*(n+1)/2];
+        }
+        void Display(bool row=true);
+        void SetRowMajor(int i,int j,int x);
+        void SetColumnMajor(int i,int j,int x);
+        int GetRowMajor(int i,int j);
+        int GetColumnMajor(int i,int j);
+        ~LTMatrix(){
+            delete []A;
+        }
+
+};
+void LTMatrix:: SetRowMajor(int i,int j,int x){
+    if(i>=j){
+        int index = ((i*(i-1))/2)+ j-1;
+        A[index]=x;
+    }
+}
+int LTMatrix:: GetRowMajor(int i,int j){
+    if(i>=j){
+        int index = ((i*(i-1))/2)+ j-1;
+        return A[index];
+    }
+    else{
+        return 0;
+    }
+}
+
+void LTMatrix:: SetColumnMajor(int i,int j,int x){
+    if(i>=j){
+        int index = n*(j-1)-(((j-2)*(j-1))/2-(i-j));
+        A[index]=x;
+    }
+}
+int LTMatrix:: GetColumnMajor(int i,int j){
+    if(i>=j){
+        int index = n*(j-1)-(((j-2)*(j-1))/2-(i-j));
+        return A[index];
+    }
+    else{
+        return 0;
+    }
+}
+void LTMatrix:: Display(bool row){
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=n; j++){
+            if (i >= j){
+                if (row){
+                    cout << GetRowMajor(i, j) << " ";
+                } else {
+                    cout << GetColumnMajor(i, j) << " ";
+                }
+            } else {
+                cout << 0 << " ";
+            }
+        }
+        cout << endl;
+    }
+ }
+
+
+
+int main(){
+    Diagonal d(4);
+    d.Set(1,1,3);
+    d.Set(2,2,5);
+    d.Set(3,3,4);
+    d.Set(0,0,7);
+    d.Display();
+
+     LTMatrix rm(4);
+    rm.SetRowMajor(1, 1, 1);
+    rm.SetRowMajor(2, 1, 2);
+    rm.SetRowMajor(2, 2, 3);
+    rm.SetRowMajor(3, 1, 4);
+    rm.SetRowMajor(3, 2, 5);
+    rm.SetRowMajor(3, 3, 6);
+    rm.SetRowMajor(4, 1, 7);
+    rm.SetRowMajor(4, 2, 8);
+    rm.SetRowMajor(4, 3, 9);
+    rm.SetRowMajor(4, 4, 10);
+ 
+    rm.Display();
+    cout << endl;
+ 
+    LTMatrix cm(4);
+    cm.SetColumnMajor(1, 1, 1);
+    cm.SetColumnMajor(2, 1, 2);
+    cm.SetColumnMajor(2, 2, 3);
+    cm.SetColumnMajor(3, 1, 4);
+    cm.SetColumnMajor(3, 2, 5);
+    cm.SetColumnMajor(3, 3, 6);
+    cm.SetColumnMajor(4, 1, 7);
+    cm.SetColumnMajor(4, 2, 8);
+    cm.SetColumnMajor(4, 3, 9);
+    cm.SetColumnMajor(4, 4, 10);
+ 
+    cm.Display(false);
+ 
 
 }
--- try.cpp
+++ try.cpp
@@ -1,37 +1,35 @@
 #include<iostream>
 using namespace std;
-int swap(int A, int B){
-    int temp=0;
-    temp=B;
-    B=A;
-    A=temp;
-    cout<<A<<" "<<B;
+void Permutation1(char S[],int k){
+    static int A[10]={0};
+    static char Res[10];
+    int i;
+    if(S[k]=='\0'){
+        Res[k]='\0'; cout<<Res<<" ";
+    }
+    else{
+        for(i=0;S[i]!='\0';i++){
+            if(A[i]==0){
+                Res[k]=S[i];
+                A[i]=1;
+                Permutation1(S,k+1);
+                A[i]=0;
+            }
+        }
+    }
+}
+void Permutation2(char S[],int l,int h){
+    if(l==h){ cout<<S<<" ";}
+    else{
+        for(int i=0;i<=h;i++){
+            swap(S[l],S[i]);
+            Permutation2(S,l+1,h);
+            swap(S[l],S[i]);
+        }
+    }
 }
 int main(){
-    int length=7; int size=10;
-    int A[size]={5,6,7,8,2,1,4};
-    for(int i=0;i<length;i++){ cout<<A[i]<<" "; }
-
-    int index;
-    cout<<"enter the index to delete: ";
-    cin>>index;
-    for(int i=index; i<length-1;i++){ A[i]=A[i+1]; }
-    length--;
-    for(int i=0;i<length;i++){ cout<<A[i]<<" "; }
-    cout<<endl;
-
-    swap(6,5);
-    int key;
-    cout<<"enter the key to be found "<<endl;
-    cin>>key;
-    for(int i=0;i<length; i++){
-        if(key==A[i]){
-            swap (A[i],A[i-1]);
-        }
-        cout<< i-1;
-    }
-
-
-
+    char S[]="abc";
+    Permutation2(S,0,2);
 return 0;
 }
