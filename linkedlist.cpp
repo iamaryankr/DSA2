750d95fa98f730dd5d1a130356f325a1d88546b0
iamaryankr <aryankumar12003@gmail.com>
2023-07-22 01:10:19 +0530

    Update linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 class Node{
@@ -24,6 +24,10 @@ class LinkedList{
         Node LSearch(int key);
         int Delete(int index);
         int length();
+        bool isSorted();
+        void RemoveDuplicate();
+        void Reverse1();
+
 
 };
 
@@ -56,34 +60,6 @@ void LinkedList:: Display(){
         p=p->next;
     }
     cout<<endl;
-}
-
-int LinkedList:: length(){
-    Node *p=first;
-    int len;
-    while(p){
-        len++;
-        p=p->next;
-    }
-    return len;
-}
-
-void LinkedList:: Insert(int index,int x){
-    Node *t, *p=first;
-    if(index<0 && index > length())  return ;
-    t=new Node;
-    t->data = x;
-    t->next = NULL;
-
-    if(index == 0){
-        t->next= first;
-        first=t;
-    }
-    else{
-        for(int i=0;i<index-1;i++)   p=p->next;
-        t->next=p->next;
-        p->next=t;
-    }
 }
 
 int LinkedList::countNodes(){
@@ -119,8 +95,109 @@ int LinkedList:: Maxdata(){
 Node LinkedList:: LSearch(int key){
     Node *p=first;
     while(p){
-        if(p->data == key) return p;
+        if(p->data == key) return *p;
         else p=p->next;
     }
-    return NULL;
+    return ;
+}
+
+int LinkedList:: length(){
+    Node *p=first;
+    int len;
+    while(p){
+        len++;
+        p=p->next;
+    }
+    return len;
+}
+
+
+void LinkedList:: Insert(int index,int x){
+    Node *t, *p=first;
+    if(index<0 && index > length())  return ;
+    t=new Node;
+    t->data = x;
+    t->next = NULL;
+
+    if(index == 0){
+        t->next= first;
+        first=t;
+    }
+    else{
+        for(int i=0;i<index-1;i++)  
+               p=p->next;
+        t->next=p->next;
+        p->next=t;
+    }
+}
+
+int LinkedList:: Delete(int index){
+    Node *p=NULL,*q=NULL;
+    int x=-1;
+    if(index<1 || index > length()) return -1;
+    if(index==1){
+        p=first;
+        first=first->next;
+        x=p->data;
+        delete p;
+    }
+    else{
+        p=first;
+        for(int i=0;i<index-1;i++){
+            q=p;
+            p=p->next;
+        }
+        q->next=p->next;
+        x=p->data;
+        delete p;
+    }
+    return x;
+}
+
+bool LinkedList:: isSorted(){
+    int x= INT32_MIN;
+    Node *p=first;
+    while(p){
+        if(p->data < x)  return false;
+        x=p->data;
+        p=p->next;
+    }
+    return true;
+}
+
+void LinkedList:: RemoveDuplicate(){
+    Node *p=first;
+    Node *q=first->next;
+    while(q){
+        if(p->data != q->data){
+            p=q;
+            q=q->next;
+        }
+        else{
+            p->next = q->next;
+            delete q;
+            q=p->next;
+        }
+    }
+    
+}
+
+void LinkedList:: Reverse1(){
+    int *A, i=0;
+    Node *p=first;
+    Node *q=p;
+    A=new int[countNodes()];
+    A=(int *)malloc(sizeof(int)*countNodes(p));
+    while(q){
+        A[i]=q->data;
+        q=q->next;
+        i++;
+    }
+    q=p;
+    i--;
+    while(q){
+        q->data=A[i];
+        q=q->next;
+        i--;
+    }
 }
