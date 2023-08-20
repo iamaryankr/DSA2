8884e0ca5e31ca19d1bb1fc3828c92500be53614
iamaryankr <aryankumar12003@gmail.com>
2023-08-21 00:20:24 +0530

    graph

---
+++ graphs/Prims.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Prims{
+    public:
+        int spanningTree(int V, vector<vector<int>> adj[]){
+        priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>> pq;
+        vector<int> vis(V, 0);
+        pq.push({0,0});
+        int sum=0;
+        while(!pq.empty()){
+            int node = pq.top().second;
+            int wt = pq.top().first;
+            pq.pop();
+            
+            if(vis[node] == 1) continue;
+            
+            vis[node] =1;
+            sum += wt;
+            for(auto it: adj[node]){
+                int adjNode = it[0];
+                int edWt = it[1];
+                if(!vis[adjNode]){
+                    pq.push({edWt, adjNode});
+                }
+            }
+        }
+        return sum;
+    }
+};
