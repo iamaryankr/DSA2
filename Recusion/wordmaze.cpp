3e3ce133b890a2b3c837722da2651884debb3400
iamaryankr <aryankumar12003@gmail.com>
2023-10-12 15:59:12 +0530

    dsa2

---
+++ Recusion/wordmaze.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int dp[301];
+    bool func(string &s, set<string> &st, int i){
+        if(i==s.size()){
+            return true;
+        }
+        if(dp[i]!=-1) return dp[i];
+        string temp;
+        for(int j=i; j<s.size(); j++){
+            temp+=s[j];
+            if(st.find(temp) != st.end()){
+                if(func(s, st, j+1)) return dp[i] = true;
+            }
+        }
+        return dp[i] = false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        set<string> st;
+        memset(dp, -1, sizeof dp);
+        for(auto it:wordDict) st.insert(it);
+        return func(s, st, 0);
+    }
+};
--- graphs/rottenoranges.cpp
+++ graphs/rottenoranges.cpp
@@ -1,38 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Sol{
+class Solution {
+public:
     int orangesRotting(vector<vector<int>>& grid) {
-        if(grid.empty()) return 0;
-        int m = grid.size(), n = grid[0].size(), days = 0, tot = 0, cnt = 0;
-        queue<pair<int, int>> rotten;
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                if(grid[i][j] != 0) tot++;
-                if(grid[i][j] == 2) rotten.push({i, j});
-            }
-        }
-        
-        int dx[4] = {0, 0, 1, -1};
-        int dy[4] = {1, -1, 0, 0};
-        
-        while(!rotten.empty()){
-            int k = rotten.size();
-            cnt += k; 
-            while(k--){
-                int x = rotten.front().first, y = rotten.front().second;
-                rotten.pop();
-                for(int i = 0; i < 4; ++i){
-                    int nx = x + dx[i];
-                    int ny = y + dy[i];
-                    if(nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] != 1) continue;
-                    grid[nx][ny] = 2;
-                    rotten.push({nx, ny});
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>vis(n, vector<int>(m,0));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]==2){
+                    vis[i][j] = 1;
+                    q.push({{i,j},0});
                 }
             }
-            if(!rotten.empty()) days++;
         }
-        
-        return tot == cnt ? days : -1;
+        int delrow[] = {1,0,-1,0};
+        int delcol[] = {0,1,0,-1};
+        int ans = 0;
+        while(!q.empty()){
+            int row = q.front().first.first; 
+            int col = q.front().first.second;
+            int t = q.front().second;
+            ans = max(ans,t);
+            q.pop();
+            for(int i=0; i<4; i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
+                grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
+                    vis[nrow][ncol]=1;
+                    grid[nrow][ncol]=2;
+                    q.push({{nrow,ncol},t+1});
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m ;j++){
+                if(grid[i][j]==1) return -1;
+            }
+        }
+        return ans;
     }
 };
